6cc6929a571153ddd441b3ef172d1df3c7e563ca
psyjc26 <psyjc26@nottingham.ac.uk>
2026-02-05 15:17:41 +0000

    Basic set up

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2B2B2B',
+        darkBlue: '#023E8A',
+        oceanBlue: '#0077B6',
+        softCyan: '#90E0EF',
+        paleBlue: '#CAF0F8'
+      }
+    },
+  },
+  plugins: [],
+}
